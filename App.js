0caf06ba1edb584cb48029c5bfabac4c8759f1f4
Cody Wall <cody@cmwall.io>
2017-09-10 10:58:09 -0500

    Connect app to API
    This commit connects the app to the API.
    
    This actually runs all the functionality. Just clean up and design from here.

--- App.js
+++ App.js
@@ -10,454 +10,126 @@ export default class App extends React.Component {
     loading: false,
     currentSearch: "",
     currentResults: [],
-
-    /*[
-      {
-        abbreviation: "CSC",
-        number: "1240",
-        hours: "3.0",
-        full_title: "Statistics and Graphics with MATLAB",
-        description:
-          "This is a General Education course. Prereq.: MATH 1021   or placement in MATH 1022 , MATH 1023 , MATH 1431 , MATH 1550  or MATH 1551 . Credit will not be given for both this course and CSC 2262  or CSC 2533  or OCS 2011 . Not for degree credit for computer science majors. 2 hrs. lecture; 2 hrs. lab.",
-        comments: [
-          "CSC 1240 - TWO EXAMS ON A TUESDAY 6:00 - 8:00PM",
-          "CSC 1240 (S1-LAB): PFT 1124-A",
-          "CSC 1240 (S2-LAB): PFT 1124-B",
-          "CSC 1240 (S3-LAB): PFT 1131",
-        ],
-        sections: [
-          {
-            number: "01",
-            title: "STATS & GRAPH MATLB",
-            enrollment_available: "54",
-            enrollment_current: "6",
-            enrollment_is_full: "false",
-            enrollment_total: "60",
-            timeIntervals: [
-              {
-                start: "1130",
-                end: "0120",
-                has_time: true,
-                days: ["FRIDAY"],
-                comments: [],
-                location_building: "",
-                location_room: "",
-                is_lab: true,
-                s_night: false,
-                s_all_web: false,
-                s_most_web: false,
-                s_half_web: false,
-                s_some_web: false,
-                s_req_dept_perm: false,
-                s_req_inst_perm: false,
-                s_majors_only: false,
-                s_cmi: false,
-                s_cmi_written: false,
-                s_cmi_spoken: false,
-                s_cmi_tech: false,
-                s_cmi_visual: false,
-                s_svc: false,
-                instructor: [
-                  {
-                    name: "BRENER N",
-                  },
-                ],
-              },
-              {
-                start: "1130",
-                end: "1220",
-                has_time: true,
-                days: ["MONDAY", "WEDNESDAY"],
-                comments: [],
-                location_building: "COATES",
-                location_room: "0152",
-                is_lab: false,
-                s_night: false,
-                s_all_web: false,
-                s_most_web: false,
-                s_half_web: false,
-                s_some_web: false,
-                s_req_dept_perm: false,
-                s_req_inst_perm: false,
-                s_majors_only: false,
-                s_cmi: false,
-                s_cmi_written: false,
-                s_cmi_spoken: false,
-                s_cmi_tech: false,
-                s_cmi_visual: false,
-                s_svc: false,
-                instructor: [
-                  {
-                    name: "BRENER N",
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            number: "02",
-            title: "STATS & GRAPH MATLB",
-            enrollment_available: "36",
-            enrollment_current: "0",
-            enrollment_is_full: "false",
-            enrollment_total: "36",
-            timeIntervals: [
-              {
-                start: "1130",
-                end: "0120",
-                has_time: true,
-                days: ["FRIDAY"],
-                comments: [],
-                location_building: "",
-                location_room: "",
-                is_lab: true,
-                s_night: false,
-                s_all_web: false,
-                s_most_web: false,
-                s_half_web: false,
-                s_some_web: false,
-                s_req_dept_perm: false,
-                s_req_inst_perm: false,
-                s_majors_only: false,
-                s_cmi: false,
-                s_cmi_written: false,
-                s_cmi_spoken: false,
-                s_cmi_tech: false,
-                s_cmi_visual: false,
-                s_svc: false,
-                instructor: [
-                  {
-                    name: "BRENER N",
-                  },
-                ],
-              },
-              {
-                start: "1130",
-                end: "1220",
-                has_time: true,
-                days: ["MONDAY", "WEDNESDAY"],
-                comments: [],
-                location_building: "COATES",
-                location_room: "0152",
-                is_lab: false,
-                s_night: false,
-                s_all_web: false,
-                s_most_web: false,
-                s_half_web: false,
-                s_some_web: false,
-                s_req_dept_perm: false,
-                s_req_inst_perm: false,
-                s_majors_only: false,
-                s_cmi: false,
-                s_cmi_written: false,
-                s_cmi_spoken: false,
-                s_cmi_tech: false,
-                s_cmi_visual: false,
-                s_svc: false,
-                instructor: [
-                  {
-                    name: "BRENER N",
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            number: "03",
-            title: "STATS & GRAPH MATLB",
-            enrollment_available: "36",
-            enrollment_current: "0",
-            enrollment_is_full: "false",
-            enrollment_total: "36",
-            timeIntervals: [
-              {
-                start: "1130",
-                end: "0120",
-                has_time: true,
-                days: ["FRIDAY"],
-                comments: [],
-                location_building: "",
-                location_room: "",
-                is_lab: true,
-                s_night: false,
-                s_all_web: false,
-                s_most_web: false,
-                s_half_web: false,
-                s_some_web: false,
-                s_req_dept_perm: false,
-                s_req_inst_perm: false,
-                s_majors_only: false,
-                s_cmi: false,
-                s_cmi_written: false,
-                s_cmi_spoken: false,
-                s_cmi_tech: false,
-                s_cmi_visual: false,
-                s_svc: false,
-                instructor: [
-                  {
-                    name: "BRENER N",
-                  },
-                ],
-              },
-              {
-                start: "1130",
-                end: "1220",
-                has_time: true,
-                days: ["MONDAY", "WEDNESDAY"],
-                comments: [],
-                location_building: "COATES",
-                location_room: "0152",
-                is_lab: false,
-                s_night: false,
-                s_all_web: false,
-                s_most_web: false,
-                s_half_web: false,
-                s_some_web: false,
-                s_req_dept_perm: false,
-                s_req_inst_perm: false,
-                s_majors_only: false,
-                s_cmi: false,
-                s_cmi_written: false,
-                s_cmi_spoken: false,
-                s_cmi_tech: false,
-                s_cmi_visual: false,
-                s_svc: false,
-                instructor: [
-                  {
-                    name: "BRENER N",
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-      {
-        abbreviation: "CSC",
-        number: "1240",
-        hours: "3.0",
-        full_title: "Statistics and Graphics with MATLAB",
-        description:
-          "This is a General Education course. Prereq.: MATH 1021   or placement in MATH 1022 , MATH 1023 , MATH 1431 , MATH 1550  or MATH 1551 . Credit will not be given for both this course and CSC 2262  or CSC 2533  or OCS 2011 . Not for degree credit for computer science majors. 2 hrs. lecture; 2 hrs. lab.",
-        comments: [
-          "CSC 1240 - TWO EXAMS ON A TUESDAY 6:00 - 8:00PM",
-          "CSC 1240 (S1-LAB): PFT 1124-A",
-          "CSC 1240 (S2-LAB): PFT 1124-B",
-          "CSC 1240 (S3-LAB): PFT 1131",
-        ],
-        sections: [
-          {
-            number: "01",
-            title: "STATS & GRAPH MATLB",
-            enrollment_available: "54",
-            enrollment_current: "6",
-            enrollment_is_full: "false",
-            enrollment_total: "60",
-            timeIntervals: [
-              {
-                start: "1130",
-                end: "0120",
-                has_time: true,
-                days: ["FRIDAY"],
-                comments: [],
-                location_building: "",
-                location_room: "",
-                is_lab: true,
-                s_night: false,
-                s_all_web: false,
-                s_most_web: false,
-                s_half_web: false,
-                s_some_web: false,
-                s_req_dept_perm: false,
-                s_req_inst_perm: false,
-                s_majors_only: false,
-                s_cmi: false,
-                s_cmi_written: false,
-                s_cmi_spoken: false,
-                s_cmi_tech: false,
-                s_cmi_visual: false,
-                s_svc: false,
-                instructor: [
-                  {
-                    name: "BRENER N",
-                  },
-                ],
-              },
-              {
-                start: "1130",
-                end: "1220",
-                has_time: true,
-                days: ["MONDAY", "WEDNESDAY"],
-                comments: [],
-                location_building: "COATES",
-                location_room: "0152",
-                is_lab: false,
-                s_night: false,
-                s_all_web: false,
-                s_most_web: false,
-                s_half_web: false,
-                s_some_web: false,
-                s_req_dept_perm: false,
-                s_req_inst_perm: false,
-                s_majors_only: false,
-                s_cmi: false,
-                s_cmi_written: false,
-                s_cmi_spoken: false,
-                s_cmi_tech: false,
-                s_cmi_visual: false,
-                s_svc: false,
-                instructor: [
-                  {
-                    name: "BRENER N",
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            number: "02",
-            title: "STATS & GRAPH MATLB",
-            enrollment_available: "36",
-            enrollment_current: "0",
-            enrollment_is_full: "false",
-            enrollment_total: "36",
-            timeIntervals: [
-              {
-                start: "1130",
-                end: "0120",
-                has_time: true,
-                days: ["FRIDAY"],
-                comments: [],
-                location_building: "",
-                location_room: "",
-                is_lab: true,
-                s_night: false,
-                s_all_web: false,
-                s_most_web: false,
-                s_half_web: false,
-                s_some_web: false,
-                s_req_dept_perm: false,
-                s_req_inst_perm: false,
-                s_majors_only: false,
-                s_cmi: false,
-                s_cmi_written: false,
-                s_cmi_spoken: false,
-                s_cmi_tech: false,
-                s_cmi_visual: false,
-                s_svc: false,
-                instructor: [
-                  {
-                    name: "BRENER N",
-                  },
-                ],
-              },
-              {
-                start: "1130",
-                end: "1220",
-                has_time: true,
-                days: ["MONDAY", "WEDNESDAY"],
-                comments: [],
-                location_building: "COATES",
-                location_room: "0152",
-                is_lab: false,
-                s_night: false,
-                s_all_web: false,
-                s_most_web: false,
-                s_half_web: false,
-                s_some_web: false,
-                s_req_dept_perm: false,
-                s_req_inst_perm: false,
-                s_majors_only: false,
-                s_cmi: false,
-                s_cmi_written: false,
-                s_cmi_spoken: false,
-                s_cmi_tech: false,
-                s_cmi_visual: false,
-                s_svc: false,
-                instructor: [
-                  {
-                    name: "BRENER N",
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            number: "03",
-            title: "STATS & GRAPH MATLB",
-            enrollment_available: "36",
-            enrollment_current: "0",
-            enrollment_is_full: "false",
-            enrollment_total: "36",
-            timeIntervals: [
-              {
-                start: "1130",
-                end: "0120",
-                has_time: true,
-                days: ["FRIDAY"],
-                comments: [],
-                location_building: "",
-                location_room: "",
-                is_lab: true,
-                s_night: false,
-                s_all_web: false,
-                s_most_web: false,
-                s_half_web: false,
-                s_some_web: false,
-                s_req_dept_perm: false,
-                s_req_inst_perm: false,
-                s_majors_only: false,
-                s_cmi: false,
-                s_cmi_written: false,
-                s_cmi_spoken: false,
-                s_cmi_tech: false,
-                s_cmi_visual: false,
-                s_svc: false,
-                instructor: [
-                  {
-                    name: "BRENER N",
-                  },
-                ],
-              },
-              {
-                start: "1130",
-                end: "1220",
-                has_time: true,
-                days: ["MONDAY", "WEDNESDAY"],
-                comments: [],
-                location_building: "COATES",
-                location_room: "0152",
-                is_lab: false,
-                s_night: false,
-                s_all_web: false,
-                s_most_web: false,
-                s_half_web: false,
-                s_some_web: false,
-                s_req_dept_perm: false,
-                s_req_inst_perm: false,
-                s_majors_only: false,
-                s_cmi: false,
-                s_cmi_written: false,
-                s_cmi_spoken: false,
-                s_cmi_tech: false,
-                s_cmi_visual: false,
-                s_svc: false,
-                instructor: [
-                  {
-                    name: "BRENER N",
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  */
+    currentDepartment: "",
+    departmentCache: [],
   }
 
   performSearch(input) {
     this.setState({ currentSearch: input, loading: true })
+
+    if (input.length < 2) {
+      this.setState({ currentResults: [], loading: false })
+      return
+    }
+
+    const lower = input.toLowerCase()
+    const inputArray = lower.split(" ")
+    const firstWord = inputArray[0]
+    const excess = inputArray.splice(1).join("")
+
+    if (this.state.currentDepartment === firstWord) {
+      this.filterCurrentDepartment(excess)
+    } else {
+      const { departmentCache } = this.state
+
+      if (departmentCache[firstWord]) {
+        this.setState({
+          currentResults: departmentCache[firstWord],
+          currentDepartment: firstWord,
+          loading: false,
+        })
+      } else {
+        this.requestDepartment(firstWord)
+      }
+    }
+  }
+
+  async requestDepartment(department) {
+    try {
+      let response = await fetch(
+        "https://lsu-api.herokuapp.com/department?dept=" + department
+      )
+      let responseJSON = await response.json()
+
+      if (responseJSON.length === 0) {
+        this.setState({ loading: false, currentResults: [] })
+      } else {
+        const departmentName = responseJSON[0].abbreviation.toLowerCase()
+
+        this.setState({
+          departmentCache: {
+            ...this.state.departmentCache,
+            [departmentName]: responseJSON,
+          },
+          currentResults: responseJSON,
+          currentDepartment: departmentName,
+          loading: false,
+        })
+      }
+    } catch (error) {
+      console.error(error)
+
+      this.setState({
+        currentResults: [],
+        currentDepartment: "",
+        loading: false,
+      })
+    }
+  }
+
+  filterCurrentDepartment(filter) {
+    const cachedDepartment = this.state.departmentCache[
+      this.state.currentDepartment
+    ]
+
+    let courses = cachedDepartment
+
+    if (filter.length > 0) {
+      if (isNaN(filter)) {
+        // return all courses whose title include filter
+
+        let filteredCourses = []
+
+        courses.forEach(course => {
+          const sections = course.sections
+          let newSections = []
+
+          sections.forEach(section => {
+            const title = section.title ? section.title.toLowerCase() : ""
+            const fullTitle = section.fullTitle
+              ? section.fullTitle.toLowerCase()
+              : ""
+
+            if (title.includes(filter) || fullTitle.includes(filter)) {
+              newSections.push(section)
+            }
+          })
+
+          if (newSections.length > 0) {
+            filteredCourses.push({ ...course, sections: newSections })
+          }
+        })
+
+        courses = filteredCourses
+      } else {
+        // return all courses whose number include filter
+
+        let filteredCourses = []
+
+        courses.forEach(course => {
+          if (course.number == filter || course.number.includes(filter)) {
+            filteredCourses.push(course)
+          }
+        })
+
+        courses = filteredCourses
+      }
+    }
+
+    this.setState({
+      currentResults: courses,
+      loading: false,
+    })
   }
 
   render() {
